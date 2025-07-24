ea470e96e8ea726d0313f04f49d88bc64b1d988a
Daws <hp@LAPTOP-SHN91JJ7.localdomain>
2025-07-24 21:38:19 +0100

    update message.hpp and scaffold of ciruclar buffer

---
+++ include/circular_buffer.hpp
@@ -0,0 +1,10 @@
+#ifndef CIRCULAR_BUFFER_HPP
+#define CIRCULAR_BUFFER_HPP
+
+#include <include/message.hpp>
+
+class CircularBuffer {
+
+};
+
+#endif
--- include/message.hpp
+++ include/message.hpp
@@ -11,7 +11,8 @@ constexpr size_t MAX_MESSAGES = 1024;
 template<typename T>
 struct alignas(64) PaddedAtomic {
     std::atomic<T> value;
-    char padding[64 - sizeof(std::atomic<T>)];
+    uint32_t size;
+    char padding[64 - sizeof(std::atomic<T>) - sizeof(uint32_t)];
 };
 
 struct SharedMemoryHeader {
@@ -21,11 +22,14 @@ struct SharedMemoryHeader {
 };
 static_assert(sizeof(SharedMemoryHeader) == 64 * 3, "SharedMemoryHeader size() must be 192 bytes");
 
-struct Message {
-    uint8_t size;
-    char size_padding[3];
-    char data[252];
+struct alignas(64) MessageSlot {
+    std::atomic<bool> ready;        
+    char padding[64 - sizeof(std::atomic<bool>)];
 };
-static_assert(sizeof(Message) == MAX_MESSAGE_SIZE, "Message size() must be 256 bytes");
+static_assert(sizeof(MessageSlot) == 64, "MessageSlot size() must be 64 bytes");
+
+struct Message {
+    char data[MAX_MESSAGE_SIZE];
+};
 
 #endif
